ecc7c8ded1b0c15186056c297f3a515607557abe
Charbel Sonon <charbelsnn@gmail.com>
2023-10-31 20:03:05 +0100

    Add Connection checking at the start

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:eventos_partenaires/pages/loginui.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:connectivity_checker/connectivity_checker.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,11 +24,27 @@ void main() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   bool? login = prefs.getBool('login');
   await FlutterConfig.loadEnvVariables();
-  runApp(login == null
-      ? MyApp1()
-      : login
-          ? MyApp()
-          : MyApp1());
+
+  final connected = await checkInternetConnection();
+  if (!connected) {
+    runApp(MyApp2());
+  } else {
+    runApp(login == null
+        ? MyApp1()
+        : login
+            ? MyApp()
+            : MyApp1());
+  }
+}
+
+Future<bool> checkInternetConnection() async {
+  final isConnected = await ConnectivityWrapper.instance.isConnected;
+  if (isConnected) {
+    return true;
+  } else {
+    // Pas de connexion Internet, vous pouvez afficher un écran d'erreur ou un message à l'utilisateur.
+    return false;
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -70,6 +87,50 @@ class MyApp1 extends StatelessWidget {
         'homepage': (context) => HomePage()
       },
       debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyApp2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Eventos',
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+        hintColor: AppColors.secondary,
+        brightness: Brightness.light,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: NotConnectedScreen(),
+      routes: {
+        'login': (context) => Login(),
+        'homepage': (context) => HomePage()
+      },
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class NotConnectedScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Connectez-vous'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Vous n'êtes pas connecté à Internet. Une fois connectée, veuillez redémarrer l'application!",
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
     );
   }
 }
